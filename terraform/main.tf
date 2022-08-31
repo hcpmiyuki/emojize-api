e56e906fc357af5a8b788c7596b9a273de350d44
hcpmiyuki <hcp.miyuki@gmail.com>
2022-08-31 18:17:54 +0900

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+resource "google_cloudbuild_trigger" "push_to_gcr_trigger" {
+  github {
+    name  = var.github_repo
+    owner = var.github_owner
+
+    push {
+      branch       = "^${var.github_branch}$"
+      invert_regex = false
+    }
+  }
+
+  build {
+    images = ["gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA"]
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = [
+        "build",
+        "-t",
+        "gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA",
+        "-f",
+        "Dockerfile",
+        "./app"
+      ]
+    }
+
+    step {
+      id   = "Push"
+      name = "gcr.io/cloud-builders/docker"
+      args = [
+        "push",
+        "gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA"
+      ]
+    }
+  }
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "emoji-ru"
+}
+
+variable "github_repo" {
+  default = "emojize-api"
+}
+
+variable "github_owner" {
+  default = "hcpmiyuki"
+}
+
+variable "github_branch" {
+  default = "main"
+}
